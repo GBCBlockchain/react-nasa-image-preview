bd27700996c1b3da5eb6e8c2708f8534c41da9ea
David Krett <dbkrett@gmail.com>
2018-11-29 20:19:24 -0500

    Components Interim

--- src/components/Imagecard.js
+++ src/components/Imagecard.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 const Imgcard = function(props) {
     return (
         <div className="card col-3">
-            <img src={props.href} alt={props.title} className="card-img-top"/>
+            <div className="card-img-top" style={{ 
+                        width: '100%', 
+                        height: '150px',
+                        backgroundImage: `url(${props.href})`, 
+                         backgroundRepeat: 'no-repeat',
+                         backgroundSize: 'contain' }} >
+            </div>
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+
+                </div>   
         </div>
     );
 };
